18d2d340f69fed32d2bdb5819c527c6a1c04224c
guttatus <xingzhiang2020@gmail.com>
2024-09-12 21:04:18 +0800

    feat: implemented reset extension

--- src/reset.rs
+++ src/reset.rs
@@ -1,29 +1,67 @@
+use core::{
+    ptr::null_mut,
+    sync::atomic::{
+        AtomicPtr,
+        Ordering::{Relaxed, Release},
+    },
+};
+use rustsbi::SbiRet;
 use sifive_test_device::SifiveTestDevice;
-use spin::Mutex;
 
-static RESET: Mutex<MachineReset> = Mutex::new(MachineReset::DeadLoop);
+pub(crate) static RESET: AtomicPtr<SifiveTestDevice> = AtomicPtr::new(null_mut());
+
+pub fn init(base: usize) {
+    RESET.store(base as _, Release);
+}
 
 pub fn fail() -> ! {
-    let lock = RESET.lock();
-    match *lock {
-        MachineReset::DeadLoop => {
-            trace!("test fail, begin dead loop");
-            loop {
-                core::hint::spin_loop()
-            }
+    let sifive_test = RESET.load(Relaxed);
+    if sifive_test.is_null() {
+        trace!("test fail, begin dead loop");
+        loop {
+            core::hint::spin_loop()
         }
-        MachineReset::SifiveTest(test) => {
-            trace!("test fail, invoke process exit procedure on SiFive Test device");
-            unsafe { &*test }.fail(0)
-        }
+    } else {
+        trace!("test fail, invoke process exit procedure on SiFive Test device");
+        unsafe { (*sifive_test).fail(0) }
     }
 }
 
-enum MachineReset {
-    DeadLoop,
-    #[allow(unused)] // TODO use on FDT parsing
-    SifiveTest(*const SifiveTestDevice),
+
+pub struct TestDevice<'a> {
+    pub sifive_test: &'a AtomicPtr<SifiveTestDevice>,
 }
 
-unsafe impl Send for MachineReset {}
-unsafe impl Sync for MachineReset {}
+impl<'a> TestDevice<'a> {
+    pub fn new(sifive_test: &'a AtomicPtr<SifiveTestDevice>) -> Self {
+        Self { sifive_test }
+    }
+}
+
+impl<'a> rustsbi::Reset for TestDevice<'a> {
+    #[inline]
+    fn system_reset(&self, reset_type: u32, reset_reason: u32) -> SbiRet {
+        use rustsbi::spec::srst::{
+            RESET_REASON_NO_REASON, RESET_REASON_SYSTEM_FAILURE, RESET_TYPE_COLD_REBOOT,
+            RESET_TYPE_SHUTDOWN, RESET_TYPE_WARM_REBOOT,
+        };
+        match reset_type {
+            RESET_TYPE_SHUTDOWN => match reset_reason {
+                RESET_REASON_NO_REASON => unsafe {
+                    (*self.sifive_test.load(Relaxed)).pass();
+                },
+                RESET_REASON_SYSTEM_FAILURE => unsafe {
+                    (*self.sifive_test.load(Relaxed)).fail(u16::MAX);
+                },
+                value => unsafe {
+                    (*self.sifive_test.load(Relaxed)).fail(value as _);
+                },
+            },
+            RESET_TYPE_COLD_REBOOT | RESET_TYPE_WARM_REBOOT => unsafe {
+                (*self.sifive_test.load(Relaxed)).reset();
+            },
+
+            _ => SbiRet::invalid_param(),
+        }
+    }
+}
