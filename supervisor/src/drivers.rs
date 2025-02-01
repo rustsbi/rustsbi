e24ac6b5c61b4ce19a66d4bac0ca25ec028959c8
Zhouqi Jiang <luojia@hust.edu.cn>
2025-02-01 10:43:20 +0800

    feat(supervisor): use ArceOS Unikernel to build a S-mode bootloader prototype
    Signed-off-by: Zhouqi Jiang <luojia@hust.edu.cn>

--- supervisor/src/drivers.rs
+++
@@ -1,3 +0,0 @@
-mod uart16550u8;
-
-pub use uart16550u8::Uart16550U8;
--- supervisor/src/main.rs
+++ supervisor/src/main.rs
@@ -1,26 +1,10 @@
-#![feature(strict_provenance)]
-#![no_std]
-#![no_main]
+#![cfg_attr(feature = "axstd", no_std)]
+#![cfg_attr(feature = "axstd", no_main)]
 
-// TODO: RustSBI EFI module
-use rcore_console::println;
+#[cfg(feature = "axstd")]
+use axstd::println;
 
-mod drivers;
-mod entry;
-mod platform;
-
-#[no_mangle]
-extern "C" fn rust_main(_hart_id: usize, _opaque: usize) {
-    platform::platform_init();
-
-    println!("Hello world!");
-    // TODO
-}
-
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    // TODO panic handler
-    loop {
-        core::hint::spin_loop();
-    }
+#[cfg_attr(feature = "axstd", no_mangle)]
+fn main() {
+    println!("Hello, world!");
 }
