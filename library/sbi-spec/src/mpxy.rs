2c45cc0600f25e61c6966419d5713d3e621a2eef
Zhenchen Wang <m202372036@hust.edu.cn>
2025-02-22 21:41:23 +0800

    sbi-spec: fix the problem that MPXY extension support does not comply with SBI standard
    Ref: https://github.com/riscv-non-isa/riscv-sbi-doc/blob/master/src/ext-mpxy.adoc
    Signed-off-by: Zhenchen Wang <m202372036@hust.edu.cn>

--- library/sbi-spec/src/mpxy.rs
+++ library/sbi-spec/src/mpxy.rs
@@ -7,32 +7,44 @@ pub use fid::*;
 
 /// Declared in §20.12.
 mod fid {
-    /// Function ID to set the shared memory for sending and receiving messages on the calling hart.
+    /// Function ID to get the version of the message proxy extension.
     ///
     /// Declared in §20.5.
-    pub const SET_SHMEM: usize = 0;
-    /// Function ID to get channel ids of the message channels accessible to the supervisor software in the shared memory of the calling hart.
+    #[doc(alias = "SBI_EXT_MPXY_GET_SHMEM_SIZE")]
+    pub const GET_SHMEM_SIZE: usize = 0;
+    /// Function ID to set the shared memory for sending and receiving messages on the calling hart.
     ///
     /// Declared in §20.6.
-    pub const GET_CHANNEL_IDS: usize = 1;
-    /// Function ID to read message channel attributes.
+    #[doc(alias = "SBI_EXT_MPXY_SET_SHMEM")]
+    pub const SET_SHMEM: usize = 1;
+    /// Function ID to get channel ids of the message channels accessible to the supervisor software in the shared memory of the calling hart.
     ///
     /// Declared in §20.7.
-    pub const READ_ATTRIBUTE: usize = 2;
-    /// Function ID to write message channel attributes.
+    #[doc(alias = "SBI_EXT_MPXY_GET_CHANNEL_IDS")]
+    pub const GET_CHANNEL_IDS: usize = 2;
+    /// Function ID to read message channel attributes.
     ///
     /// Declared in §20.8.
-    pub const WRITE_ATTRIBUTE: usize = 3;
+    #[doc(alias = "SBI_EXT_MPXY_READ_ATTRS")]
+    pub const READ_ATTRIBUTE: usize = 3;
+    /// Function ID to write message channel attributes.
+    ///
+    /// Declared in §20.9.
+    #[doc(alias = "SBI_EXT_MPXY_WRITE_ATTRS")]
+    pub const WRITE_ATTRIBUTE: usize = 4;
     /// Function ID to send a message to the mpxy channel and waits for sbi implementation for the message response.
     ///
-    /// Declared in 20.9.
-    pub const SEND_MESSAGE_WITH_RESPONSE: usize = 4;
+    /// Declared in 20.10.
+    #[doc(alias = "SBI_EXT_MPXY_SEND_MSG_WITH_RESP")]
+    pub const SEND_MESSAGE_WITH_RESPONSE: usize = 5;
     /// Function ID to send a message to the mpxy channel and does not waits for response.
     ///
-    /// Declared in 20.10.
-    pub const SEND_MESSAGE_WITHOUT_RESPONSE: usize = 5;
+    /// Declared in 20.11.
+    #[doc(alias = "SBI_EXT_MPXY_SEND_MSG_WITHOUT_RESP")]
+    pub const SEND_MESSAGE_WITHOUT_RESPONSE: usize = 6;
     /// Function ID to get the message protocol specific notification events on the mpxy channel.
     ///
-    /// Declared in 20.11.
-    pub const GET_NOTIFICATION_EVENTS: usize = 6;
+    /// Declared in 20.12.
+    #[doc(alias = "SBI_EXT_MPXY_GET_NOTIFICATION_EVENTS")]
+    pub const GET_NOTIFICATION_EVENTS: usize = 7;
 }
