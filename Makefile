b61a759dcccebdfa47681091b2063f5b0a6dda87
JingXuan_Wei <137764115+JensenWei007@users.noreply.github.com>
2025-05-26 09:09:22 +0800

    feat(main): Modify the operating mode and run it in payload mode (#3)
    - Change build.sh to use payload mode
    - Add rustsbi repo when build this project
    
    Signed-off-by: JensenWei007 <jensenwei007@gmail.com>

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-BOOT:= arceboot.bin
 DISK:= fat32_disk_test.img
+SBI:=rustsbi/target/riscv64imac-unknown-none-elf/release/rustsbi-prototyper-payload.elf
 
 OUT_CONFIG ?= $(PWD)/.axconfig.toml
 
@@ -7,10 +7,10 @@ export AX_CONFIG_PATH=$(OUT_CONFIG)
 
 clean:
 	cargo clean
-	rm arceboot.bin
+	cd rustsbi && cargo clean
 
-build:
+build: clean
 	./build.sh
 
 run:
-	qemu-system-riscv64 -serial mon:stdio -bios rustsbi-prototyper.bin -nographic -machine virt -kernel $(BOOT) -device virtio-blk-pci,drive=disk0 -drive id=disk0,if=none,format=raw,file=$(DISK)
+	qemu-system-riscv64 -serial mon:stdio -bios $(SBI) -nographic -machine virt -device virtio-blk-pci,drive=disk0 -drive id=disk0,if=none,format=raw,file=$(DISK)
--- build.sh
+++ build.sh
@@ -1,18 +1,47 @@
 #!/bin/bash
 set -e
 
+# 预定义变量与函数
 TARGET=riscv64gc-unknown-none-elf
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+ELF=$SCRIPT_DIR/target/$TARGET/release/arceboot
+BIN=$SCRIPT_DIR/target/$TARGET/release/arceboot.bin
+SBI=$SCRIPT_DIR/rustsbi/target/riscv64imac-unknown-none-elf/release/rustsbi-prototyper-payload.elf
 
-ELF=target/$TARGET/release/arceboot
+print_info() {
+    printf "\033[1;37m%s\033[0m" "[RustSBI-Arceboot Build] "
+    printf "\033[1;32m%s\033[0m" "[INFO] "
+    printf "\033[36m%s\033[0m\n" "$1"
+}
 
 # 编译
+print_info "开始编译 ArceBoot..."
 cargo rustc --release --target $TARGET -- \
     -C opt-level=z \
     -C panic=abort \
     -C relocation-model=static \
     -C target-cpu=generic \
     -C link-args="-Tlink.ld"
+print_info "编译 ArceBoot 成功"
 
-rust-objcopy --binary-architecture=riscv64 $ELF --strip-all -O binary arceboot.bin
+# 提取 binary
+print_info "开始提取 ArceBoot Binary..."
+rust-objcopy --binary-architecture=riscv64 $ELF --strip-all -O binary $BIN
+print_info "提取 ArceBoot Binary 成功"
 
-echo "BIOS binary generated: arceboot.bin"
+# 检查并克隆 rustsbi 仓库
+# TODO: 后面合入 rustsbi 后这段需要改
+if [ ! -d "rustsbi" ]; then
+    print_info "仓库 rustsbi 不存在，正在克隆仓库..."
+    git clone https://github.com/rustsbi/rustsbi.git
+    print_info "仓库 rustsbi 克隆仓库成功"
+else
+    print_info "仓库 rustsbi 已存在"
+fi
+
+# 编译 rustsbi
+print_info "开始以 payload 形式编译 rustsbi..."
+cd rustsbi && cargo prototyper --payload $BIN
+print_info "rustsbi 编译成功"
+
+print_info "所有任务执行完成,项目构建成功,目标 elf 文件位于: $SBI"
