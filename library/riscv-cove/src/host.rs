60d40e20ce4adf97433832e4874015e0ffcb0f3c
Zhouqi Jiang <luojia@hust.edu.cn>
2025-08-03 14:39:12 +0800

    feat(cove): add riscv-cove-rt CoVE S-mode runtime module
    Signed-off-by: Zhouqi Jiang <luojia@hust.edu.cn>

--- library/riscv-cove/src/host.rs
+++ library/riscv-cove/src/host.rs
@@ -108,3 +108,60 @@ mod fid {
     #[doc(alias = "SBI_EXT_COVH_TVM_REMOVE_PAGES")]
     pub const TVM_REMOVE_PAGES: usize = 19;
 }
+
+/// Possible state of a TEE Security Manager (TSM).
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+#[repr(u32)]
+pub enum TsmState {
+    /// TSM has not been loaded on this platform.
+    NotLoaded = 0,
+    /// TSM has been loaded, but has not yet been initialized.
+    Loaded = 1,
+    /// TSM has been loaded & initialized, and is ready to accept ECALLs.
+    Ready = 2,
+}
+
+// TODO generic type of T replacing `usize`s, see sbi_spec::binary::SbiRet
+/// Information structure of a TEE Security Manager (TSM).
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+#[repr(C)]
+pub struct TsmInfo {
+    /// The raw current state of the TSM.
+    ///
+    /// If the state is not [`Ready`], the remanining fields are invalid
+    /// and will be initialized to zero.
+    ///
+    /// [`Ready`]: struct.TsmState.html#Ready
+    pub tsm_state: u32,
+    /// Identifier of the TSM implementation.
+    ///
+    /// This identifier is intended to distinguish among different TSM
+    /// implementations, potentially managed by different organizations,
+    /// that might target different deployment models and, thus,
+    /// implement subset of CoVE specification.
+    pub tsm_impl_id: u32,
+    /// Version number of the running TSM.
+    pub tsm_version: u32,
+    /// A bit mask of CoVE features supported by the running TSM.
+    ///
+    /// Every bit in this field corresponds to a capability defined by
+    /// constants. Presense of bit `i` indicates that both the TSM and
+    /// hardware support the corresponding capability.
+    pub tsm_capabilities: usize,
+    /// The number of 4-KiB pages which must be donated to the TSM for
+    /// storing TVM state in `covh_create_tvm_vcpu`.
+    ///
+    /// `0` if the TSM does not support the dynamic memory allocation
+    /// capability.
+    pub tvm_state_pages: usize,
+    /// The maximum number of vCPUs a TVM can support.
+    pub tvm_max_vcpus: usize,
+    /// The number of 4-KiB pages which must be donated to the TSM when
+    /// creating a new vCPU.
+    ///
+    /// `0` if the TSM does not support the dynamic memory allocation
+    /// capability.
+    pub tvm_vcpu_state_pages: usize,
+}
+
+// TODO unit tests on offsets of TsmInfo.
