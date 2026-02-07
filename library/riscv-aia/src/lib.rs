f92ff77ae7d9a6d949f379918c194ed1a9a9d350
Zhouqi Jiang <luojia@rustsbi.com>
2026-02-07 14:14:33 +0800

    feat(aia): add standard RISC-V interrupt constants for `riscv_aia::Iid`
    Signed-off-by: Zhouqi Jiang <luojia@rustsbi.com>

--- library/riscv-aia/src/lib.rs
+++ library/riscv-aia/src/lib.rs
@@ -27,6 +27,29 @@ use core::num::NonZeroU16;
 /// assert!(Iid::new(0).is_none());
 /// assert!(Iid::new(3000).is_none());
 /// ```
+///
+/// ```
+/// # use riscv_aia::Iid;
+/// # mod mtopi {
+/// # use riscv_aia::Iid;
+/// # pub struct Mtopi;
+/// # impl Mtopi { pub fn iid(&self) -> Option<Iid> { None } }
+/// # pub fn read() -> Mtopi { Mtopi }
+/// # }
+/// // Read AIA interrupt ID from `mtopi` register.
+/// // On each read, `mtopi` returns the next interrupt we should process.
+/// while let Some(iid) = mtopi::read().iid() {
+///     if iid == Iid::MTIMER {
+///         // Handle machine timer interrupt in SBI implementation.
+///     } else if iid == Iid::MSOFT {
+///         // Handle machine software interrupt in SBI implementation.
+///     } else if iid == Iid::MEXT {
+///         // Handle machine external interrupt in SBI implementation.
+///     } else {
+///         // Default interrupt handler.
+///     }
+/// }
+/// ```
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 #[repr(transparent)]
 pub struct Iid {
@@ -34,6 +57,24 @@ pub struct Iid {
 }
 
 impl Iid {
+    /// `Iid` for Supervisor software interrupt in standard RISC-V.
+    pub const SSOFT: Iid = Iid::new(1).unwrap();
+
+    /// `Iid` for Machine software interrupt in standard RISC-V.
+    pub const MSOFT: Iid = Iid::new(3).unwrap();
+
+    /// `Iid` for Supervisor timer interrupt in standard RISC-V.
+    pub const STIMER: Iid = Iid::new(5).unwrap();
+
+    /// `Iid` for Machine timer interrupt in standard RISC-V.
+    pub const MTIMER: Iid = Iid::new(7).unwrap();
+
+    /// `Iid` for Supervisor external interrupt in standard RISC-V.
+    pub const SEXT: Iid = Iid::new(9).unwrap();
+
+    /// `Iid` for Machine external interrupt in standard RISC-V.
+    pub const MEXT: Iid = Iid::new(11).unwrap();
+
     /// Attempts to construct an [`Iid`] from `number`.
     ///
     /// Returns `Some(Iid)` when `1 <= number <= 2047`; returns `None` if
@@ -69,5 +110,46 @@ mod tests {
         assert!(Iid::new(1).is_some());
         assert!(Iid::new(2047).is_some());
         assert!(Iid::new(2048).is_none());
+    }
+
+    #[test]
+    fn iid_consts() {
+        assert_eq!(Iid::SSOFT.number(), 1);
+        assert_eq!(Iid::MSOFT.number(), 3);
+        assert_eq!(Iid::STIMER.number(), 5);
+        assert_eq!(Iid::MTIMER.number(), 7);
+        assert_eq!(Iid::SEXT.number(), 9);
+        assert_eq!(Iid::MEXT.number(), 11);
+    }
+
+    #[test]
+    fn iid_usage_match_if() {
+        let iid = Iid::MEXT;
+        if iid == Iid::MTIMER {
+            unreachable!()
+        } else if iid == Iid::MSOFT {
+            unreachable!()
+        } else if iid == Iid::MEXT {
+            assert!(true)
+        } else {
+            unreachable!()
+        }
+
+        match iid {
+            Iid::MTIMER => unreachable!(),
+            Iid::MSOFT => unreachable!(),
+            Iid::MEXT => assert!(true),
+            _ => unreachable!(),
+        }
+
+        // Mock `mtopi::read` result where `iid: Option<Iid>`.
+        let iid = Some(Iid::MSOFT);
+        match iid {
+            Some(Iid::MTIMER) => unreachable!(),
+            Some(Iid::MSOFT) => assert!(true),
+            Some(Iid::MEXT) => unreachable!(),
+            // Redundant, can be `_ => unreachable!()` in real use.
+            Some(_) | None => unreachable!(),
+        }
     }
 }
