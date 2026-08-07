3d1bae50b5f2bffac34ad94cd3f84cb6c7218bb0
Zhouqi Jiang <luojia@rustsbi.com>
2026-08-07 10:56:46 +0800

    doc(aia): include README.md as crate level document
    Signed-off-by: Zhouqi Jiang <luojia@rustsbi.com>

--- library/riscv-aia/src/lib.rs
+++ library/riscv-aia/src/lib.rs
@@ -1,7 +1,4 @@
-//! Rust support for RISC-V Advanced Interrupt Architecture (AIA).
-//!
-//! This crate follows _The RISC-V Advanced Interrupt Architecture_ specification, Version 1.0, Revised 20250312.
-
+#![doc = include_str!("../README.md")]
 #![no_std]
 
 mod iid;
